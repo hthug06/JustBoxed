cd42117db54e736e0c09e6ccd27a99e5fd066cc5
ht06 <hthug06@gmail.com>
2025-01-14 15:56:37 +0100

    feat: add a visit system /bx visit <player>

--- src/main/java/fr/ht06/justBoxed/Events/PlayerListeners.java
+++ src/main/java/fr/ht06/justBoxed/Events/PlayerListeners.java
@@ -8,7 +8,11 @@ import org.bukkit.GameRule;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
 
 public class PlayerListeners implements Listener {
     BoxManager boxManager = JustBoxed.manager;
@@ -46,6 +50,93 @@ public class PlayerListeners implements Listener {
                     box.broadcastMessage(Component.text("Advancement complete: ").append(event.getAdvancement().displayName()));
                 }
             }
+        }
+    }
+
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        //We didn't care about the main world
+        if (player.getWorld().getName().equals("world") || player.getWorld().getName().equals("world_nether") || player.getWorld().getName().equals("world_the_end")) {
+            return;
+        }
+
+        //if the player didn't have a box, he can't interact
+        if(!boxManager.hasBox(player.getUniqueId())) {
+            player.sendMessage("§cVous ne pouvez pas interagir avec des block qui ne sont pas dans votre box.");
+            event.setCancelled(true);
+            return;
+        }
+
+        Box box = boxManager.getBoxByPlayer(player.getUniqueId());
+
+        //if the player is not on his box, he can't interact
+        if (!player.getWorld().getName().equals(box.getWorldName())){
+            player.sendMessage("§cVous ne pouvez pas interagir avec des block qui ne sont pas dans votre box.");
+            event.setCancelled(true);
+            return;
+        }
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+
+        //We didn't care about the main world
+        if (player.getWorld().getName().equals("world") || player.getWorld().getName().equals("world_nether") || player.getWorld().getName().equals("world_the_end")) {
+            return;
+        }
+
+        //if the player didn't have a box, he can't interact
+        if(!boxManager.hasBox(player.getUniqueId())) {
+            player.sendMessage("§cVous ne pouvez pas interagir avec des block qui ne sont pas dans votre box.");
+            event.setCancelled(true);
+            return;
+        }
+
+        Box box = boxManager.getBoxByPlayer(player.getUniqueId());
+
+        //if the player is not on his box, he can't interact
+        if (!player.getWorld().getName().equals(box.getWorldName())){
+            player.sendMessage("§cVous ne pouvez pas interagir avec des block qui ne sont pas dans votre box.");
+            event.setCancelled(true);
+            return;
+        }
+    }
+
+    @EventHandler
+    public void onHit(EntityDamageEvent event) {
+        //event.getEntity()  -> l'entité tapé
+        //event.getDamageSource().getCausingEntity() ->  l'attaquant
+
+        //if this isnot a player, we don't care
+        if (!(event.getDamageSource().getCausingEntity() instanceof Player)){
+            return;
+        }
+
+        Player player = (Player) event.getDamageSource().getCausingEntity();
+
+        //We didn't care about the main world
+        if (player.getWorld().getName().equals("world") || player.getWorld().getName().equals("world_nether") || player.getWorld().getName().equals("world_the_end")) {
+            return;
+        }
+
+        //if the player didn't have a box, he can't interact
+        if(!boxManager.hasBox(player.getUniqueId())) {
+            player.sendMessage("§cVous ne pouvez pas interagir avec des block qui ne sont pas dans votre box.");
+            event.setCancelled(true);
+            return;
+        }
+
+        Box box = boxManager.getBoxByPlayer(player.getUniqueId());
+
+        //if the player is not on his box, he can't interact
+        if (!player.getWorld().getName().equals(box.getWorldName())){
+            player.sendMessage("§cVous ne pouvez pas interagir avec des block qui ne sont pas dans votre box.");
+            event.setCancelled(true);
+            return;
         }
     }
 }
